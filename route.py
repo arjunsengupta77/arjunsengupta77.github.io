de3b7cd81d5e66c9517bbcd58ce6a4408e9672e3
arjunsengupta77 <114380029+arjunsengupta77@users.noreply.github.com>
2025-01-22 13:34:10 +0530

    Update route.py

--- route.py
+++ route.py
@@ -1,79 +1,62 @@
-import plotly.graph_objects as go
-import networkx as nx
 from flask import Blueprint, render_template
-from app import db  # Assuming you're using SQLAlchemy for your models
-from app.models import Attribute, System, AttributeSystemMapping  # Your actual models
+from .models import db, System, Attribute, AttributeSystemMapping
+import networkx as nx
+import plotly.graph_objects as go
 
-# Create the blueprint for lineage routes
 lineage_bp = Blueprint('lineage', __name__)
 
 # Helper function to generate the Plotly graph
 def generate_lineage_graph(nodes, edges):
-    """
-    Generate a Plotly graph from the nodes and edges of a lineage.
+    # Create a Plotly figure
+    fig = go.Figure()
 
-    :param nodes: List of nodes (systems)
-    :param edges: List of edges (relationships between systems)
-    :return: Plotly figure object
-    """
-    edge_x = []
-    edge_y = []
+    # Add nodes to the figure
+    node_x = [node['label'] for node in nodes]
+    node_y = [i for i in range(len(nodes))]
+    
+    fig.add_trace(go.Scatter(
+        x=node_x,
+        y=node_y,
+        mode='markers+text',
+        text=node_x,
+        marker=dict(size=12, color='blue'),
+        textposition="top center"
+    ))
+
+    # Add edges (connections between nodes) to the figure
     for edge in edges:
-        x0, y0 = edge[0]['pos']
-        x1, y1 = edge[1]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_y.append(y0)
-        edge_y.append(y1)
+        source = edge['source']
+        target = edge['target']
+        fig.add_trace(go.Scatter(
+            x=[source, target],
+            y=[node_y[node_x.index(source)], node_y[node_x.index(target)]],
+            mode='lines',
+            line=dict(width=2, color='gray')
+        ))
 
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines'
-    )
-
-    node_x = []
-    node_y = []
-    node_text = []
-    for node in nodes:
-        x, y = node['pos']
-        node_x.append(x)
-        node_y.append(y)
-        node_text.append(node['label'])
-
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        marker=dict(
-            showscale=True,
-            colorscale='YlGnBu',
-            size=10
-        ),
-        text=node_text
-    )
-
-    layout = go.Layout(
-        title='Lineage Visualization',
-        showlegend=False,
-        hovermode='closest',
-        xaxis=dict(showgrid=False, zeroline=False),
-        yaxis=dict(showgrid=False, zeroline=False),
-    )
-
-    fig = go.Figure(data=[edge_trace, node_trace], layout=layout)
+    fig.update_layout(showlegend=False, xaxis=dict(showgrid=False), yaxis=dict(showgrid=False))
+    
     return fig
 
-# Route for glossary
+# Glossary route: lists attributes with their definitions and origin systems
 @lineage_bp.route('/glossary')
 def glossary():
     attributes = Attribute.query.all()
-    return render_template('glossary.html', attributes=attributes)
 
-# Route for full lineage (systems only)
+    glossary_data = []
+    for attribute in attributes:
+        glossary_data.append({
+            'name': attribute.attribute_name,
+            'description': attribute.description,
+            'origin_system': attribute.origin_system
+        })
+
+    return render_template('glossary.html', glossary_data=glossary_data)
+
+# Full Lineage route: Displays all systems in the lineage
 @lineage_bp.route('/full_lineage')
 def full_lineage():
+    # Query all systems
     systems = System.query.all()
 
     # Create a directed graph with NetworkX
@@ -81,53 +64,62 @@ def full_lineage():
 
     # Add nodes (systems)
     for system in systems:
-        G.add_node(system.system_name, pos=(system.system_id, system.system_id), label=system.system_name)
+        G.add_node(system.system_name, label=system.system_name)
 
-    # Add edges (lineages between systems)
-    attribute_system_mappings = AttributeSystemMapping.query.all()  # This represents the relationship between systems and attributes
-    for attribute_system_mapping in attribute_system_mappings:
-        source_system = attribute_system_mapping.system.system_name
-        target_system = attribute_system_mapping.attribute.origin_system
+    # Query all attribute-system mappings, joining with systems and attributes
+    attribute_system_mappings = db.session.query(AttributeSystemMapping, System).join(System).all()
+
+    # Add edges (lineages between systems based on AttributeSystemMapping)
+    for mapping, system in attribute_system_mappings:
+        source_system = system.system_name  # System where the attribute originates
+        target_system = mapping.attribute.origin_system  # Origin system of the attribute
+        
+        # Add edge if source and target systems are different
         if source_system != target_system:
             G.add_edge(source_system, target_system)
 
     # Generate Plotly graph
-    nodes = [{'label': system['label'], 'pos': system['pos']} for system in G.nodes(data=True)]
-    edges = [{'source': G.nodes[edge[0]], 'target': G.nodes[edge[1]]} for edge in G.edges()]
-    
+    nodes = [{'label': system['label']} for system in G.nodes(data=True)]
+    edges = [{'source': edge[0], 'target': edge[1]} for edge in G.edges()]
+
     fig = generate_lineage_graph(nodes, edges)
 
     # Return the rendered template with Plotly graph
     graph_html = fig.to_html(full_html=False)
     return render_template('lineage_full.html', graph_html=graph_html)
 
-# Route for specific attribute lineage
-@lineage_bp.route('/attribute_lineage/<attribute_id>')
+# Attribute-specific Lineage route: Displays lineage of a given attribute
+@lineage_bp.route('/attribute_lineage/<int:attribute_id>')
 def attribute_lineage(attribute_id):
     attribute = Attribute.query.get_or_404(attribute_id)
 
+    # Query systems associated with the given attribute
+    attribute_system_mappings = AttributeSystemMapping.query.filter_by(attribute_id=attribute_id).all()
+    
     # Create a directed graph with NetworkX
     G = nx.DiGraph()
 
-    # Add nodes (systems related to the attribute)
-    attribute_system_mappings = AttributeSystemMapping.query.filter_by(attribute_id=attribute_id).all()
-    for attribute_system_mapping in attribute_system_mappings:
-        system = attribute_system_mapping.system
-        G.add_node(system.system_name, pos=(system.system_id, system.system_id), label=system.system_name)
+    # Add nodes (systems)
+    systems = set()
+    for mapping in attribute_system_mappings:
+        systems.add(mapping.system.system_name)
+        G.add_node(mapping.system.system_name)
 
-    # Add edges (lineages between systems)
-    for attribute_system_mapping in attribute_system_mappings:
-        source_system = attribute_system_mapping.system.system_name
-        target_system = attribute_system_mapping.attribute.origin_system
+    # Add edges (lineages between systems based on AttributeSystemMapping)
+    for mapping in attribute_system_mappings:
+        source_system = mapping.system.system_name
+        target_system = mapping.attribute.origin_system
+        
+        # Add edge if source and target systems are different
         if source_system != target_system:
             G.add_edge(source_system, target_system)
 
     # Generate Plotly graph
-    nodes = [{'label': system['label'], 'pos': system['pos']} for system in G.nodes(data=True)]
-    edges = [{'source': G.nodes[edge[0]], 'target': G.nodes[edge[1]]} for edge in G.edges()]
+    nodes = [{'label': system['label']} for system in G.nodes(data=True)]
+    edges = [{'source': edge[0], 'target': edge[1]} for edge in G.edges()]
 
     fig = generate_lineage_graph(nodes, edges)
 
     # Return the rendered template with Plotly graph
     graph_html = fig.to_html(full_html=False)
-    return render_template('lineage_attribute.html', attribute=attribute, graph_html=graph_html)
+    return render_template('attribute_lineage.html', attribute=attribute, graph_html=graph_html)
